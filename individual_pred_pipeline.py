40e9d00ea0f0f46d66ee30fc27762ca8ea427689
csernac0 <cristina.serna@justo.mx>
2021-04-22 20:01:22 -0500

    adding documentation

--- individual_pred_pipeline.py
+++ individual_pred_pipeline.py
@@ -5,12 +5,19 @@ from src.train import TrainModel
 from src.eval import EvalModel
 
 if __name__ == '__main__':
+	#PIPELINE STARTS
+    #Instanciate PrepareRawData class
     obj_prepare = PrepareRawData()
+    #read downloaded raw data
     obj_prepare.get_raw(file_name = 'train.csv', raw_id = int(sys.argv[1]))
+    #Classify numerical & categorical features  
     obj_prepare.get_numeric_categoric_vars()
+    #Processing categorical features  
     obj_prepare.process_categoric(raw_id = True)
+    #Processing numerical nan imputation
     obj_prepare.impute_numerical(raw_id = True)
 
+    #Eval model
     obs_to_eval = obj_prepare.get_single_obs()
     obj_eval = EvalModel(single_obs = obs_to_eval)
     obj_eval.eval_model()
